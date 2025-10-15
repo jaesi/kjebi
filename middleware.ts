6e917194e4ec5ac584ae30fb878674e36e1b7308
Jaessi <answotlr54@snu.ac.kr>
2025-10-15 19:40:54 +0900

    fix: syntax error

--- middleware.ts
+++ middleware.ts
@@ -26,7 +26,7 @@ export async function middleware(req: NextRequest) {
   }
 
   return NextResponse.next();
-});
+};
 
 export const config = {
   matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
